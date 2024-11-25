0265e0eb9e888243b09db838343603163bfe6ece
Filip Dziedzic <dziedzicfilip00@gmail.com>
2024-11-25 16:35:12 +0100

    Dodawanie powoli widoku dla wyswietlanie projektów

---
+++ ProjectManagementApp/ViewModels/WszystkieProjketyViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementApp.ViewModels
+{
+    public class WszystkieProjketyViewModel : WorkspaceViewModel
+    {
+        public WszystkieProjketyViewModel() 
+        
+        {
+            base.DisplayName = "Towary";
+        }
+    }
+}
