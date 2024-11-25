1000b9fcbc3e1782f56d4cc05d9b482bb4f2cdb4
dzied <dziedzicfilip00@gmail.com>
2024-11-25 11:34:31 +0100

    Dodaj pliki projektów.

---
+++ ProjectManagementApp/ViewModels/NowyTowarViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectManagementApp.ViewModels
+{
+    public class NowyTowarViewModel : WorkspaceViewModel
+    {
+        public NowyTowarViewModel()
+        {
+            base.DisplayName = "Towar";
+        }
+    }
+}
