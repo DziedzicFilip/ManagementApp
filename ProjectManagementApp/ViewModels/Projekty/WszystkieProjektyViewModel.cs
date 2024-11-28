e80c51c1cb47bd1eaaf940b6e3c000e1c052dce9
Filip Dziedzic <dziedzicfilip00@gmail.com>
2024-11-28 22:02:26 +0100

    Poczatek wyswietlania bazy dancyh

--- ProjectManagementApp/ViewModels/Projekty/WszystkieProjektyViewModel.cs
+++ ProjectManagementApp/ViewModels/Projekty/WszystkieProjektyViewModel.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections.ObjectModel;
+using ProjectManagementApp.Models.Entities;
 
 namespace ProjectManagementApp.ViewModels
 {
     public class WszystkieProjektyViewModel : WorkspaceViewModel
     {
+        #region Fields
+        private readonly ZarzadanieProjektami2Entities zarzadanieProjektami2Entities;
+        #endregion
+        #region Properties
+
+        #endregion
+        #region Constructor
         public WszystkieProjektyViewModel()
         {
             base.DisplayName = "Projkety";
 
         }
+        #endregion
+        #region Helpers
+
+        #endregion
+
     }
 }
