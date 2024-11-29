c9ac9bb278496b6e6979babe4407d5d404d46d82
Filip Dziedzic <dziedzicfilip00@gmail.com>
2024-11-29 20:56:08 +0100

    dodanie wyswietlania z bazy

--- ProjectManagementApp/ViewModels/Projekty/WszystkieProjektyViewModel.cs
+++ ProjectManagementApp/ViewModels/Projekty/WszystkieProjektyViewModel.cs
@@ -4,26 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Collections.ObjectModel;
 using ProjectManagementApp.Models.Entities;
+using ProjectManagementApp.Helper;
+using System.Windows.Input;
 
 namespace ProjectManagementApp.ViewModels
 {
     public class WszystkieProjektyViewModel : WorkspaceViewModel
     {
-        #region Fields
+        #region DB
         private readonly ZarzadanieProjektami2Entities zarzadanieProjektami2Entities;
+        
+        
         #endregion
-        #region Properties
+        #region LoadCommand
+        private BaseCommand _LoadCommand;
+        public ICommand LoadCommand
+        {
+            get
+            {
+                if(_LoadCommand == null)
+                
+                    _LoadCommand = new BaseCommand(() => load());
+                return _LoadCommand;   
+                
+            }
+                
 
+        }
+        #endregion
+        #region List
+        private ObservableCollection<Projekty> _List;
+        public ObservableCollection<Projekty> List
+        {
+            get {
+                if (_List == null)
+                {
+                    load();
+                }
+                return _List;
+            }
+            set
+            {
+                _List = value;
+                OnPropertyChanged(() => List);
+            }
+        }
         #endregion
         #region Constructor
         public WszystkieProjektyViewModel()
         {
             base.DisplayName = "Projkety";
-
+            zarzadanieProjektami2Entities = new ZarzadanieProjektami2Entities();
         }
         #endregion
         #region Helpers
-
+        private void load()
+        {
+            List = new ObservableCollection<Projekty>
+                (
+                   zarzadanieProjektami2Entities.Projekty.ToList()
+                );
+        }
         #endregion
 
     }
