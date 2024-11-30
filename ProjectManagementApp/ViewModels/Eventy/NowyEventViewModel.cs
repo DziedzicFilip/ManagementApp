f9211388bf0c8012d58b11f1abc006a5deb5297c
Filip Dziedzic <dziedzicfilip00@gmail.com>
2024-11-30 02:49:51 +0100

    Eventy z czasem

--- ProjectManagementApp/ViewModels/Eventy/NowyEventViewModel.cs
+++ ProjectManagementApp/ViewModels/Eventy/NowyEventViewModel.cs
@@ -76,14 +76,74 @@ namespace ProjectManagementApp.ViewModels
 
             }
         }
+        public int? Godzina_rozpoczeciaStart
+        {
+            get => item?.data_rozpoczecia?.Hour;
+            set
+            {
+                if (value.HasValue && item.data_rozpoczecia.HasValue)
+                {
+                   
+                    item.data_rozpoczecia = item.data_rozpoczecia.Value.AddHours(value.Value - item.data_rozpoczecia.Value.Hour);
+                    OnPropertyChanged(() => Godzina_rozpoczeciaStart);
+                    OnPropertyChanged(() => Data_rozpoczecia);
+                }
+            }
+        }
 
+        public int? Minuta_rozpoczeciaStart
+        {
+            get => item?.data_rozpoczecia?.Minute;
+            set
+            {
+                if (value.HasValue && item.data_rozpoczecia.HasValue)
+                {
+                    
+                    item.data_rozpoczecia = item.data_rozpoczecia.Value.AddMinutes(value.Value - item.data_rozpoczecia.Value.Minute);
+                    OnPropertyChanged(() => Minuta_rozpoczeciaStart);
+                    OnPropertyChanged(() => Data_rozpoczecia);
+                }
+            }
+        }
 
+        public int? Godzina_zakonczeniaEnd
+        {
+            get => item?.data_zakonczenia?.Hour;
+            set
+            {
+                if (value.HasValue && item.data_zakonczenia.HasValue)
+                {
+                    item.data_zakonczenia = item.data_zakonczenia.Value.AddHours(value.Value - item.data_zakonczenia.Value.Hour);
+                    OnPropertyChanged(() => Godzina_zakonczeniaEnd);
+                    OnPropertyChanged(() => Data_zakonczenia);
+                }
+            }
+        }
+
+        public int? Minuta_zakonczeniaEnd
+        {
+            get => item?.data_zakonczenia?.Minute;
+            set
+            {
+                if (value.HasValue && item.data_zakonczenia.HasValue)
+                {
+                    item.data_zakonczenia = item.data_zakonczenia.Value.AddMinutes(value.Value - item.data_zakonczenia.Value.Minute);
+                    OnPropertyChanged(() => Minuta_zakonczeniaEnd);
+                    OnPropertyChanged(() => Data_zakonczenia);
+                }
+            }
+        }
 
         #endregion
 
         #region Helpers
+
+
+
         public override void ADD()
         {
+
+          
 
             zarzadanieProjektami2Entities.Wydarzenia.Add(item);
             zarzadanieProjektami2Entities.SaveChanges();
